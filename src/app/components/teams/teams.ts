0db630cd34bf79cbe040d647acdbf377eeb8162b
Mishal-malayil <mishalsonu2003@gmail.com>
2025-11-27 20:15:22 +0530

    view add team

--- src/app/components/teams/teams.ts
+++ src/app/components/teams/teams.ts
@@ -1,11 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-teams',
-  imports: [],
+  standalone: true,
+  imports: [MatIconModule,CommonModule,MatCardModule],
   templateUrl: './teams.html',
   styleUrl: './teams.css',
 })
 export class Teams {
+teams:any[]=[];
+
+ constructor() {
+    this.loadTeams();
+  }
+
+  // Load from localStorage
+  loadTeams() {
+    const savedTeams = localStorage.getItem('teams');
+    if (savedTeams) {
+      this.teams = JSON.parse(savedTeams);
+    } else {
+      // Optional: Default teams when no data
+      this.teams = [
+        {
+          
+        },
+        {
+        
+        }
+      ];
+    }
+  }
 
 }
