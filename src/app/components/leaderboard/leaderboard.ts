1dee2d4557962f75279e6b06bc7e964f27a0d580
Mohammed Mishal M <mishalsonu2003@gmail.com>
2025-12-23 20:03:14 +0530

    Update leaderboard.ts

--- src/app/components/leaderboard/leaderboard.ts
+++ src/app/components/leaderboard/leaderboard.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-leaderboard',
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './leaderboard.html',
   styleUrl: './leaderboard.css',
 })
