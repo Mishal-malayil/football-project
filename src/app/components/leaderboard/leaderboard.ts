77de35a7dc386cfd2fb4e7c54cc462393e39a3de
Mohammed Mishal M <mishalsonu2003@gmail.com>
2025-12-27 10:18:21 +0530

    Update leaderboard.ts

--- src/app/components/leaderboard/leaderboard.ts
+++ src/app/components/leaderboard/leaderboard.ts
@@ -7,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './leaderboard.css',
 })
 export class Leaderboard {
+leaderboard = [
+  {
+    TeamName: 'FC Tigers',
+    TeamLogo: 'tigers.png',
+    Played: 5,
+    Won: 4,
+    Draw: 1,
+    Lost: 0,
+    Goals: 12,
+    Points: 13
+  }
+];
 
 }
